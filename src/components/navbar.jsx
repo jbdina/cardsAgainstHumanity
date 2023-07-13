d1d42780fea0f4970d105725c981341da8197d33
Lenovo Yoga 520 <leonie.weinert@student.hs-anhalt.de>
2023-07-13 17:55:10 +0200

    navbar verbessert

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 //import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
 import logo from "../../src/assets/hslogo.jpg"
 
-const Menu = () => (
+/*const Menu = () => (
     <div className="menu">
-        { <p><a href="#about">About</a></p> }
-        { <p><a href="#cardsAgainstHumanity">Cards Against Humanity</a></p> }
-        { <p><a href="#help">Help</a></p> }
+         <p><a href="#about">About</a></p>
+         <p><a href="#cardsAgainstHumanity">Cards Against Humanity</a></p>
+         <p><a href="#help">Help</a></p>
 
     </div>
 );
+
+ */
+
+
 const Navbar = () => {
    // const [toggleMenu,setToggleMenu] = useState(false);
     return (
@@ -19,8 +23,10 @@ const Navbar = () => {
                 <div className='cards__navbar-links_logo'>
                     <img src={logo} alt="logo"/>
                 </div>
-                <div className='cards__navbar-links-container'>
-                    <Menu/>
+                <div className='cards__navbar-links_container'>
+                    <p><a href="#about">About</a></p>
+                    <p><a href="#cardsagainsthumanity">Cards Against Humanity</a></p>
+                    <p><a href="#help">Help</a></p>
                 </div>
             </div>
 
@@ -29,4 +35,4 @@ const Navbar = () => {
         </div>
     )
 }
-export default Navbar;
+export default Navbar
