f2b97d4ddb1ed3f17d3fe5545a307dbb743619b3
sarinahaupt <sarina.haupt@student.hs-anhalt.de>
2023-07-13 18:27:17 +0200

    header bearbeitet

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div className="CAH__footer section__padding">
+        <div className="CAH__footer-heading">
+            <h1 className="gradient__text">A party game for horrible people</h1>
+            <p>This project was created at Anhalt University under the supervision of Toni Barth.</p>
+
+             <div className="CAH__footer-content">
+                 <p> Developer</p>
+                 <p>Jamila Gränzer		jamila.graenzer@student.hs-anhalt.de</p>
+                 <p>Leonie Weinert		leonie.weinert@student.hs-anhalt.de</p>
+                 <p>Sarina Haupt	     sarina.haupt@student.hs-anhalt.de</p>
+             </div>
+        </div>
+
+        </div>
+    )
+}
+
+export default Footer
