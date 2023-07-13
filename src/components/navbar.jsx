530dc284b94ca1b426b0e112f174dc0552c9dc3d
Lenovo Yoga 520 <leonie.weinert@student.hs-anhalt.de>
2023-07-13 16:26:47 +0200

    navbar erweitert

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import React, { useState } from 'react'
+//import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
 import logo from "../../src/assets/hslogo.jpg"
 
-const Menu = () => {
-    <>
-    <p><a href="#about">About</a></p>
+const Menu = () => (
+    <div className="menu">
+        { <p><a href="#about">About</a></p> }
+        { <p><a href="#cardsAgainstHumanity">Cards Against Humanity</a></p> }
+        { <p><a href="#help">Help</a></p> }
 
-    </>
-}
+    </div>
+);
 const Navbar = () => {
+    const [toggleMenu,setToggleMenu] = useState(false);
     return (
         <div className='cards__navbar'>
             <div className='cards__navbar-links'>
                 <div className='cards__navbar-links_logo'>
                     <img src={logo} alt="logo"/>
                 </div>
-                <div className='cards__navbar_navbar-links-container'>
+                <div className='cards__navbar-links-container'>
                     <Menu/>
                 </div>
             </div>
+
+
+
         </div>
     )
 }
+export default Navbar;
