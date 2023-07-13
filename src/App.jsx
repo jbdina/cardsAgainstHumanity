401a5d9bc2fcdb1ff23724282b880b92913c4f36
sarinahaupt <sarina.haupt@student.hs-anhalt.de>
2023-07-13 18:27:39 +0200

    header bearbeitet

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import GameContainer from './containers/gameContainer/gameContainer';
+import Header from './containers/header/header';
+import Footer from './containers/footer/Footer';
 import './App.css';
-import {PlayerContainer} from "./containers/index";
 
 export default class App extends Component {
 render(){
     return(
     <h1> Wir sind bereit für unser Spiel</h1>,
     <GameContainer />,
-        <PlayerContainer />
+        <Header />,
+        <Footer />
     )
 }
 }
