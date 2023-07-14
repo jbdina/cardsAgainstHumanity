e332ef8cd8c50fbc65136808b88d39f69fe9404a
DragonNextGen <jberyldinah@gmail.com>
2023-07-15 00:31:52 +0200

    navbar optimiert, header an mobile version angepasst, überprüfen einiger funktionen, finden neuer fehler leider.

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -6,17 +6,17 @@ const Header = () => {
         <div className="CAH__header__padding" id="about">
             <div className="CAH__header-content">
                 <h1 className="gradient__text">Cards Against Humanity</h1>
-                <p>We are excited to present Cards Against Humanity, a web project created as part of our course in web and media programming at Anhalt University of Applied Sciences. Our goal was to develop a responsive webpage that brings the popular game to life in a digital format, allowing users to enjoy it anytime, anywhere.
-
-                    Cards Against Humanity is an incredibly entertaining and humorous party game. Players take turns filling in the blanks of statements or questions using cards with various phrases or words. The objective is to create the most outrageous, clever, and often controversial combinations.
-
-                    Whether you're a seasoned player or new to the game, our university project offers an immersive and entertaining way to enjoy Cards Against Humanity. Get ready to laugh, challenge your sense of humor, and create memorable moments with friends or even strangers online.
-
+                <p>           </p>
+                <p>We are excited to present Cards Against Humanity, a web project created as part of our course in web and media programming at Anhalt University of Applied Sciences. Our goal was to develop a responsive webpage that brings the popular game to life in a digital format, allowing users to enjoy it anytime, anywhere.</p>
+                <p>
                     Join us on this digital journey as we bring Cards Against Humanity to a whole new level. Have fun, embrace your creativity, and let the laughter begin!
-                        Note: This Site is still in development phase</p>
+                </p>
+
+                <p>         </p>
+                <p><strong>Note: This site is still in the development phase.</strong></p>
             </div>
         </div>
     )
 }
 
-export default Header
+export default Header;
